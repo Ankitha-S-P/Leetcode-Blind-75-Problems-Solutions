04bf42a53541c1de4b24ab37b060518e256aa186
ankitha-p <ankithashenoy003@gmail.com>
2024-09-10 21:34:15 +0530

    Delete Extras/maximum depth.py

--- Extras/maximum depth.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def depth(node):
-            if node is None:
-                return 0
-            height=1+max(depth(node.left),depth(node.right))
-            return height
-        return depth(root)    
